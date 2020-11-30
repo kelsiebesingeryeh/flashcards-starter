59d126b0a68e0b6a369deac2fef5839bfb4166b9
Kelsie Besinger <kelsiebesinger@gmail.com>
2020-11-30 16:07:45 -0700

    Add turn class file to begin writing tests

---
+++ src/Turn.js
@@ -0,0 +1,10 @@
+const card = require('./Card');
+
+class Turn {
+  constructor(guess, card) {
+    this.guess = guess;
+    this.card = new Card(id, question, answers, correctAnswer);
+  }
+
+}
+module.exports = Turn;
