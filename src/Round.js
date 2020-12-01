0874e55b45f9ccb7973de464c1048362fc835a96
Kelsie Besinger <kelsiebesinger@gmail.com>
2020-11-30 20:44:04 -0700

    Add testing functionality for round class

--- src/Round.js
+++ src/Round.js
@@ -1,5 +1,43 @@
-class Round {
+const Turn = require('./Turn');
 
+//outstanding questions:
+// 1. how do we get the currentCard to be the first card in the deck at the start of the round??
+// 2. review the takeTurn method and test assertions for that
+// 3. 
+
+class Round {
+  constructor() {
+    this.deck = new Deck();
+    this.turns = 0;
+    this.currentCard = //current card?
+    this.incorrectGuesses = [];
+  }
+
+  returnCurrentCard() {
+    //method that return the current card being played.
+    return this.currentCard;
+  }
+
+  takeTurn() {
+    const turn = new Turn(); //when a guess is made, create new Turn instance
+    this.turns++;
+    this.returnCurrentCard(); //next card becomes the current card
+    turn.evaluateGuess(); // guess is evaluated/recorded. incorrect guesses will be stored via the id in an array of incorrect guesses
+    if (!turn.evaluateGuess()) {
+      this.incorrectGuess.push(turn.guess)
+    }
+    turn.giveFeedback();
+    // feedback is returned regarding whether the guess is incorrect or correct.
+  }
+  calculatePercentCorrect() {
+    this.incorrectGuesses.length / this.turns;
+    // method that calculates and returns the percentage of correct guesses
+    // something that counts the length of the array and divides by the total amount of turns
+  }
+
+  endRound() {
+    return `** Round over! ** You answered ${this.calculatePercentCorrect()} % of the questions correctly!`;
+  }
 }
 
 module.exports = Round;
--- test/Round-test.js
+++ test/Round-test.js
@@ -18,6 +18,44 @@ describe('Round', () => {
     expect(round).to.be.an.instanceof(Round);
   });
 
+  it('should return current card being played', () => {
+    const card1 = new Card(2, 'What is a comma-separated list of related values?', ["array", "object", "function"], "array");
+    const card2 = new Card(4,'What type of prototype method does not modify the existing array but returns a particular representation of the array?', ["mutator method", "accessor method", "iteration method"], 'accessor method');
+    const card3 = new Card(6, 'What is an example of a mutator method?', ["sort()", "map()", "join()"], 'sort()');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    round.returnCurrentCard();
+    expect(round.returnCurrentCard()).to.equal(this.currentCard);
+  })
 
+  it('should create a new Turn instance when a guess is made', () => {
+    const round = new Round();
+    round.takeTurn();
+    expect(round.takeTurn()).to.be.an.instanceof(Turn);
+  });
+
+  it('should update the turn count regardless if guess is correct or incorrect', () => {
+    const round = new Round();
+    round.takeTurn();
+    expect(round.turns)to.equal(1);
+  });
+
+  it('should return the next card as the current card', () => {
+    const round = new Round();
+    round.takeTurn();
+    expect(this.currentCard).to.equal() // the next card?
+  });
+
+  it('should add incorrect guesses to an array', () => {
+    const round = new Round();
+    round.takeTurn();
+    expect(round.incorrectGuesses).to.deep.equal(['spleen']);
+  });
+
+  it('should return feedback on the guess', () => {
+    const round = new Round;
+    round.takeTurn();
+    expect(round.)
+  })
 
 })
