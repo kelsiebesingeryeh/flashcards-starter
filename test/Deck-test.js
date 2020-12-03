0f01f7791d486cdb1c7e3cc232162016f7af8a60
Kelsie Besinger <kelsiebesinger@gmail.com>
2020-12-03 15:28:43 -0700

    Refactor deck class test file

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -7,9 +7,13 @@ const Deck = require('../src/Deck');
 
 describe('Deck', () => {
   let deck;
+  let card1, card2, card3;
 
   beforeEach( () => {
     deck = new Deck();
+    card1 = new Card(2, 'What is a comma-separated list of related values?', ["array", "object", "function"], "array");
+    card2 = new Card(4,'What type of prototype method does not modify the existing array but returns a particular representation of the array?', ["mutator method", "accessor method", "iteration method"], 'accessor method');
+    card3 = new Card(6, 'What is an example of a mutator method?', ["sort()", "map()", "join()"], 'sort()');
   });
 
   it('should be a function', () => {
@@ -20,12 +24,13 @@ describe('Deck', () => {
     expect(deck).to.be.an.instanceof(Deck);
   });
 
+  it('should hold an array of cards by default', () => {
+    deck = new Deck([card1, card2, card3]);
+    expect(deck.cards).to.deep.equal([card1, card2, card3]);
+  });
+
   it('should count the cards in the deck', () => {
-    const card1 = new Card(2, 'What is a comma-separated list of related values?', ["array", "object", "function"], "array");
-    const card2 = new Card(4,'What type of prototype method does not modify the existing array but returns a particular representation of the array?', ["mutator method", "accessor method", "iteration method"], 'accessor method');
-    const card3 = new Card(6, 'What is an example of a mutator method?', ["sort()", "map()", "join()"], 'sort()');
     const deck = new Deck([card1, card2, card3]);
-    deck.countCards();
-    expect(deck.countCards()).to.equal(3)
+    expect(deck.countCards()).to.deep.equal(3);
   });
 });
